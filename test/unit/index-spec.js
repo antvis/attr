7c0e0be2db03e11a8796953ac7e72af6c5f26286
simaQ <sima.zhang1990@gmail.com>
2018-06-24 10:03:34 +0800

    add test case

--- test/unit/index-spec.js
+++ test/unit/index-spec.js
@@ -1,9 +1,8 @@
 const expect = require('chai').expect;
-const attr = require('../../src/index');
+const Attr = require('../../src/index');
 
-describe('sample', () => {
-  it('attr', () => {
-    expect('attr').to.be.a('string');
-    expect(attr).to.be.an('object');
+describe('Attr', () => {
+  it('Attr', () => {
+    expect(Attr).to.include.all.keys('Position', 'Shape', 'Color', 'Size', 'Opacity', 'ColorUtil');
   });
 });
